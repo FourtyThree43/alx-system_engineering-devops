79debb5c565df88dcb7910b8dc18ef55896e2c50
FourtyThree43 <shaqmwa@outlook.com>
2023-05-03 16:49:19 +0300

    Updated

--- 0x06-regular_expressions/6-phone_number.rb
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
 # Script that accepts one argument and pass it to a regular expression matching method.
-puts ARGV[0].scan(/^\d{10,10}/).join
+puts ARGV[0].scan(/^\d{10,10}$/).join
