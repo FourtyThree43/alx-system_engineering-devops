85b420de76d43db63a41aa10c36fd4d46d5dc684
FourtyThree43 <shaqmwa@outlook.com>
2023-05-03 18:25:27 +0300

    ADV

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# Script that accepts one argument and pass it to a regular expression matching method.
+#puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
+
+log_line = ARGV[0]
+
+if match = log_line.match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
+    sender = match[1]
+    receiver = match[2]
+    flags = match[3]
+    puts "#{sender},#{receiver},#{flags}"
+  else
+    puts "No match found"
+  end
