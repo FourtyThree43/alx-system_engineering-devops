54f6083ce4ee7d03a199ce99c7a8adb8340f38db
FourtyThree43 <shaqmwa@outlook.com>
2023-07-03 20:46:54 +0300

    ADv 2.0 - Timestamp: 1688406414

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,37 @@
 # Install Nginx package
 package { 'nginx':
-  ensure => 'installed',
+  ensure => installed,
 }
 
-# Configure Nginx with custom HTTP response header
-file { '/etc/nginx/conf.d/custom_headers.conf':
-  ensure  => 'file',
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => 'add_header X-Served-By $hostname;',
+# Replace Nginx default configuration
+file { '/etc/nginx/nginx.conf':
+  ensure  => file,
+  source  => 'puppet:///modules/nginx/nginx.conf',
+  require => Package['nginx'],
   notify  => Service['nginx'],
 }
 
-# Ensure Nginx service is running and enabled
-service { 'nginx':
-  ensure  => 'running',
-  enable  => true,
+# Create a custom Nginx configuration file
+file { '/etc/nginx/conf.d/custom_header.conf':
+  ensure  => file,
+  content => 'server {
+                listen 80 default_server;
+                listen [::]:80 default_server;
+                server_name _;
+                location / {
+                  add_header X-Served-By $hostname;
+                  root /var/www/html;
+                  index index.html;
+                }
+              }',
   require => Package['nginx'],
+  notify  => Service['nginx'],
+}
+
+# Restart Nginx service when configuration changes
+service { 'nginx':
+  ensure    => running,
+  enable    => true,
+  require   => Package['nginx'],
+  subscribe => File['/etc/nginx/nginx.conf', '/etc/nginx/conf.d/custom_header.conf'],
 }
