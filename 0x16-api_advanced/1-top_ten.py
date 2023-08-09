c0fa94e39e90f60db959ee6a354af57014b3b5c1
FourtyThree43 <shaqmwa@outlook.com>
2023-08-09 11:42:19 +0300

    Query str:/hot.json?limit=10

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""
+0-top_ten
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    """
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    headers = {"User-Agent": "Python/requests"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        try:
+            results = response.json().get("data")
+            [
+                print(c.get("data").get("title"))
+                for c in results.get("children")
+            ]
+
+        except KeyError:
+            return 0
+    else:
+        return 0
