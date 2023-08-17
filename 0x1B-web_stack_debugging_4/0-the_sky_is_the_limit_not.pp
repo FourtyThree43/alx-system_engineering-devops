36182c734fbb3132a7efa1c8d7f1d27c9f514c45
FourtyThree43 <shaqmwa@outlook.com>
2023-08-17 12:29:05 +0300

    update the ulimit and restart Nginx

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,24 @@
+# Puppet Manifest: 0-the_sky_is_the_limit_not.pp
+# This manifest manages the ulimit configuration for Nginx and ensures the Nginx service is running.
+
+class nginx_ulimit {
+
+  # Set the ulimit value
+  $ulimit_value = "-n 4096"
+
+  # Define the exec resource to update the ulimit and restart Nginx
+  exec { 'update_ulimit_and_restart_nginx':
+    command     => "/bin/sed -i 's/^ULIMIT=.*/ULIMIT=\"$ulimit_value\"/' /etc/default/nginx",
+    path        => '/bin:/usr/bin',
+    refreshonly => true,
+    subscribe   => Service['nginx'],
+  }
+
+  # Define a service resource to manage Nginx
+  service { 'nginx':
+    ensure    => running,
+    enable    => true,
+  }
+}
+
+include nginx_ulimit
