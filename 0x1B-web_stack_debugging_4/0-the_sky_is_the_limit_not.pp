c7b141516378f5db2d7036f993875abc57dd8f36
FourtyThree43 <shaqmwa@outlook.com>
2023-08-17 14:01:23 +0300

    The universe is possible

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,28 +1,22 @@
 # Puppet Manifest: 0-the_sky_is_the_limit_not.pp
 # This manifest manages the ulimit configuration for Nginx and ensures the Nginx service is running.
 
-class nginx_ulimit {
+$ulimit_value = '-n 4096'
 
-  # Set the ulimit value
-  $ulimit_value = "-n 4096"
-
-  # Define the exec resource to update the ulimit and restart Nginx
-  exec { 'update_ulimit_and_restart_nginx':
-    provider => shell,
-    command  => "sudo sed -i 's/^ULIMIT=.*/ULIMIT=\"$ulimit_value\"/' /etc/default/nginx",
-    before   => Exec['restart'],
-  }
-
-  exec {'restart':
-    provider    => shell,
-    command     => 'sudo service nginx restart',
-    subscribe   => Service['nginx'],
-  }
-
-  service { 'nginx':
-    ensure    => running,
-    enable    => true,
-  }
+# Define the exec resource to update the ulimit and restart Nginx
+exec { 'update_ulimit_and_restart_nginx':
+  provider => shell,
+  command  => "sudo sed -i 's/^ULIMIT=.*/ULIMIT=\"${ulimit_value}\"/' /etc/default/nginx",
+  before   => Exec['restart'],
 }
 
-include nginx_ulimit
+exec {'restart':
+  provider  => shell,
+  command   => 'sudo service nginx restart',
+  subscribe => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
